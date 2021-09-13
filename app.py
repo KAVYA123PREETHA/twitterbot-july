1e57e9f2fcc621062ad8275e0adc91c45b50c7cb
KAVYA123PREETHA <87599003+KAVYA123PREETHA@users.noreply.github.com>
2021-09-13 20:36:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,64 @@
-# Final Code
-import time
-import tweepy
 import os
+pip install adafruit-io
 
-consumer_key = os.getenv('consumer_key')
-consumer_secret=os.getenv('consumer_secret')
-access_token=os.getenv('access_token')
-access_token_secret=os.getenv('access_token_secret')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+pip install python-telegram-bot==13.0 --quiet
 
-api = tweepy.API(auth)
-print("Authentication approved")
-while True:
-  user = api.get_user('@Ameen91741779')
-  u = user.followers_count
-  api.update_profile(name=f'AMEER {u} Followers')
-  print(f'AMEER {u} Followers')
-  time.sleep(60)
+
+from Adafruit_IO import Client
+aio = Client(os.getenv('uname'),os.getenv('feed'))
+
+
+from telegram.ext import Updater , MessageHandler , Filters
+
+def demo1(bot,update):
+  
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning the lights on')
+  aio.send('light',1)
+  
+  
+def demo2(bot,update):
+  
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning the lights off')
+  aio.send('light',0)
+  
+  
+def demo3(bot,update):
+  
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning the fan on')
+  aio.send('fan',1)
+  
+  
+def demo4(bot,update):
+  
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('turning the fan off')
+  aio.send('fan',0)
+  
+  
+
+def main(bot,update):
+  a = bot.message.text
+  if a =='Turn on light':
+   demo1(bot,update)
+   
+  elif a =='Turn off light':
+   demo2(bot,update)
+   
+  elif a =='Turn on fan':
+   demo3(bot,update)
+   
+  elif a =='Turn off fan':
+   demo4(bot,update)
+
+bot_token = os.getenv('bot_token’)
+u = Updater(bot_token,use_context=True)
+dp = u.dispatcher
+dp.add_handler(MessageHandler(Filters.text,main))
+u.start_polling()
+u.idle()
  
   
